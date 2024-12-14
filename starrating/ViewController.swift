336326d49d731cf48b89839ae52a22b20cf37015
John F <john@johnscode.com>
2024-12-14 15:59:48 -0600

    Initial Commit

---
+++ starrating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  starrating
+//
+//  Created by John Fowler on 12/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ starratingTests/starratingTests.swift
@@ -0,0 +1,17 @@
+//
+//  starratingTests.swift
+//  starratingTests
+//
+//  Created by John Fowler on 12/14/24.
+//
+
+import Testing
+@testable import starrating
+
+struct starratingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
